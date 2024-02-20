e07a8bcae3fef53a2d821e7aa9ef43e2c4d54116
LucasFrigo <lukeg299@gmail.com>
2024-02-20 11:38:20 -0300

    Added hitbox prefab creation for blocks using Shape2D

--- Resources/Scripts/BlockScript.gd
+++ Resources/Scripts/BlockScript.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 # Adds both resources in the node Inspector to quickly assign them.
 @export var block_texture: Texture2D
-@export var hitbox_block: CollisionShape2D
+@export var hitbox_shape: Shape2D
 
 @onready var rigid_body = $RigidBody2D
-@onready var mesh = $MeshInstance2D
 @onready var sprite = $Sprite2D
+@onready var hitbox = $RigidBody2D/CollisionShape2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite.texture = block_texture
+	hitbox.shape = hitbox_shape
 	pass
 	
 
